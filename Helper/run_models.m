c0321f77b4a69c67875798ea479e21cada2434b5
zhilk <zoe.hilker@maxplanckschools.de>
2026-09-08 17:09:09 +0200

    remove conditioning line, keep internal to model fits

--- Helper/run_models.m
+++ Helper/run_models.m
@@ -17,7 +17,6 @@ function allStudyResults = run_models(behavioralData, studyLabels)
 %    allStudyResults – 3x1 cell array of per-study result structs
 
 nStudies = numel(behavioralData);
-behavioralData = addConditioningMeans(behavioralData); % add mean from conditioning phase to initiate weights later
 allStudyResults = cell(nStudies, 1);
 
 for s = 1:nStudies
